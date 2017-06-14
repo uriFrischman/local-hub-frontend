9dbf26f32782944617b361c108ce443c85119e1a
Uri <uri.frischman@gmail.com>
2017-06-14 13:28:07 -0400

    working add item, need to fix add alert

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 /**
  * Created by Uri on 2017-06-13.
  */
+
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 34.397, lng: 150.644 },
+        scrollwheel: false,
+        zoom: 2
+    });
+    $.get("http://local-hub.herokuapp.com/api/items", function (data) {
+        data.forEach(function (item) {
+            var position = {lat: item.location.geolocation[0], lng: item.location.geolocation[1]};
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: item.name
+            });
+        })
+    })
+
+
+}
+
+var GOOGLE_MAPS_API_KEY = "AIzaSyDslgvFhh-aCwWDyk-A6fBMgU6wrrR95f8";
+
+
 var app = angular.module('localHubApp', ['localHubAppModals', 'localHubDirectives']);
 
 // app.config(['$qProvider', function ($qProvider) {
@@ -14,119 +39,129 @@ app.controller('localHubController', function ($scope, myModal, $http) {
    $scope.alertsModal = myModal.newModal('modal', 'localHubController');
    $scope.itemsModal = myModal.newModal('items-modal/items-modal', 'localHubController');
 
-
+   $scope.searchForDevicesData = "";
 
    $scope.openAlertsModal = function () {
-       // $scope.alertsModal = myModal.newModal('modal', 'localHubController');
-       // console.log($scope);
-       // $scope.showModal = $scope.alertsModal.activate();
        $scope.alertsModal.activate();
-
    };
 
    $scope.itemsModalInfo = {};
-
-   $scope.addNewItem = function () {
-       console.log($scope.itemsModalInfo)
-   };
+   $scope.alertModalInfo = {};
 
    $scope.openItemsModal = function () {
        $scope.itemsModal.activate();
-   }
+   };
 
    $scope.closeItemsModal = function () {
         $scope.itemsModal.deactivate();
-   }
+   };
 
    $scope.closeAlertsModal = function () {
+       $scope.alertsModal.deactivate();
 
-   }
+   };
 
-   $scope.devices = $http.get('http://local-hub.herokuapp.com/api/items').then(function (data) {
-       console.log(data.data);
-   });
+   $scope.devices = [];
+   $scope.getDevices = function () {
+       $http.get('http://local-hub.herokuapp.com/api/items').then(function (data) {
+           $scope.devices = data.data;
+           console.log($scope.devices)
+       });
+   };
+   $scope.getDevices();
 
-   $scope.addItem = function () {
-   //    http://local-hub.herokuapp.com/api/item POST
-   //    _id, name, latitude, longitude, type_of_device
-   //     $http.post("http://local-hub.herokuapp.com/api/item",{
-   //         "_id": "123",
-   //         "name": "Test name",
-   //         "latitude": "34.532",
-   //         "longitude": "45.32",
-   //         "type_of_device": "phone"
-   //     });
+    function getRandomInRange(from, to, fixed) {
+        return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+        // .toFixed() returns string, so ' * 1' is a trick to convert to number
+    }
 
-       var data = {
-           _id: "123",
-           name: "Test name",
-           latitude: 34.532,
-           longitude: 45.32,
-           type_of_device: "phone"
+   $scope.addItem = function (data) {
+           var formattedData = {
+               name: data.name,
+               type_of_device: data.type_of_device,
+               _id: data.id,
+               longitude: getRandomInRange(-180, 180,3).toString(),
+               latitude: getRandomInRange(-180, 180,3).toString()
+               // longitude: "40.7589",
+               // latitude: "73.9851"
+           };
+           console.log(formattedData);
+           $.post("http://local-hub.herokuapp.com/api/item", formattedData);
+           $scope.getDevices();
+   };
+
+   $scope.addAlert = function (data) {
+
+       var items = {};
+       data.items.forEach(function (item, index) {
+           // items[index.toString()] = JSON.parse(item);
+           // items[index.toString()] = item;
+           item = JSON.parse(item);
+           items[index.toString()] = {"_id": item._id, "name": item.name, "item_longitude": item.location.geolocation[1].toString(), "item_latitude": item.location.geolocation[0].toString(), "type_of_device" :item.type_of_device}
+       });
+
+
+       var times = {
+           "0": data.timeFrom.getHours().toString() + ":" + (data.timeFrom.getMinutes()<10?'0':'') + data.timeFrom.getMinutes(),
+           "1": data.timeTo.getHours().toString() + ":" + (data.timeTo.getMinutes()<10?'0':'') + data.timeTo.getMinutes()
        };
 
-       // var xhr = new XMLHttpRequest();
-       // xhr.open('POST', 'http://local-hub.herokuapp.com/api/item');
-       // xhr.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
-       // xhr.onload = function() {
-       //     if (xhr.status === 200) {
-       //         var userInfo = JSON.parse(xhr.responseText);
-       //     }
-       // };
-       // xhr.send(JSON.stringify(data));
-
-
-       $http({
-           method: 'POST',
-           url: "http://local-hub.herokuapp.com/api/item",
-           data: data,
-           headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-
-       }).then(function (data) {
-           console.log(data);
-       });
-       console.log("added item");
+        var formattedData = {
+            name: data.name,
+            distance: data.distanceInMeters.toString(),
+            items: items,
+            alert_latitude: getRandomInRange(-180,180,3).toString(),
+            alert_longitude: getRandomInRange(-180, 180,3).toString(),
+            times:times
+        };
+        $.post("http://local-hub.herokuapp.com/api/alert", formattedData);
+        console.log("formatteddata", formattedData);
    };
 
 
-   $scope.sampleDevices = [
-       {
-          kind: 'car',
-          name: "John's Car",
-          location: '175 Main Street',
-          needToAlert: true
-       },
-       {
-           kind: 'wallet',
-           name: "Bill's Wallet",
-           location: '175 Main Street',
-           needToAlert: true
-       },
-       {
-           kind: 'keys',
-           name: "Wayne's Keys",
-           location: '175 Main Street',
-           needToAlert: false
-       },
-       {
-           kind: 'phone',
-           name: "Sam's Nexus 5",
-           location: '175 Main Street',
-           needToAlert: false
-       },
-       {
-           kind: 'car',
-           name: "Bruce's Van",
-           location: '175 Main Street',
-           needToAlert: true
-       },
-       {
-           kind: 'phone',
-           name: "Josh's iPhone",
-           location: '175 Main Street',
-           needToAlert: false
-       }
-   ];
+
+
+
+   // $scope.sampleDevices = [
+   //     {
+   //        kind: 'car',
+   //        name: "John's Car",
+   //        location: '175 Main Street',
+   //        needToAlert: true
+   //     },
+   //     {
+   //         kind: 'wallet',
+   //         name: "Bill's Wallet",
+   //         location: '175 Main Street',
+   //         needToAlert: true
+   //     },
+   //     {
+   //         kind: 'keys',
+   //         name: "Wayne's Keys",
+   //         location: '175 Main Street',
+   //         needToAlert: false
+   //     },
+   //     {
+   //         kind: 'phone',
+   //         name: "Sam's Nexus 5",
+   //         location: '175 Main Street',
+   //         needToAlert: false
+   //     },
+   //     {
+   //         kind: 'car',
+   //         name: "Bruce's Van",
+   //         location: '175 Main Street',
+   //         needToAlert: true
+   //     },
+   //     {
+   //         kind: 'phone',
+   //         name: "Josh's iPhone",
+   //         location: '175 Main Street',
+   //         needToAlert: false
+   //     }
+   // ];
+
+
 
 
 
